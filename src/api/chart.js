768819f8b00d19bb256c22b6faf9b0ac499d9385
Sherlockcmd <921277132@qq.com>
2025-10-21 19:25:40 +0800

    修改了注释

--- src/api/chart.js
+++ src/api/chart.js
@@ -1,5 +1,5 @@
 import service from '@/utils/request.js'
-// 将所有的针对user的请求，都封装到一个对象中
+// 单独封装对于统计图表的请求
 const chartApi = {
     selectBanjiCount() {
         return service.get('/chart/selectBanjiCount', {})
